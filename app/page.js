2f9f8923bb24bbb5545dc18c7a8e6a102548f9d9
lamiaaibrahim <lamiaelhooby@gmail.com>
2024-03-07 18:29:16 +0200

    Remove the header component from the page.

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import Link from "next/link";
 
-import Header from "./header";
+import Header from "@/components/header";
 export default function Home() {
   console.log("Test");
   return (
     <main>
       <Header />
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
         <Link href="/about"> About us</Link>
