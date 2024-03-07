0f5e91f7fd84440367ce7ee0a7c297429d5c0b29
lamiaaibrahim <lamiaelhooby@gmail.com>
2024-03-07 18:21:16 +0200

    Add Header component to the page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
+
+import Header from "./header";
 export default function Home() {
   console.log("Test");
   return (
     <main>
+      <Header />
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
